dada21e01e3deb13f032cdbb66a248ebf6676f40
vaishnavi7-stack <vaishisbest7@gmail.com>
2026-06-30 16:52:19 +0530

    login

--- build.sh
+++ build.sh
@@ -4,3 +4,18 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create a superuser automatically on first deploy if env vars are set
+# and no superuser exists yet. Safe to run on every deploy.
+python manage.py shell -c "
+from django.contrib.auth.models import User
+import os
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f'Superuser {username} created.')
+else:
+    print('Superuser already exists or env vars not set, skipping.')
+"
